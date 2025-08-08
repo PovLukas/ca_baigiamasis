28bb1038209a6ec8a2f481414d7629566f31dcb7
Lukas <lukpov@yahoo.com>
2025-08-08 14:11:19 +0300

    registration complete

--- frontend/pages/register/index.tsx
+++ frontend/pages/register/index.tsx
@@ -1,28 +1,63 @@
 import Header from "@/components/Header/Header";
-import React, { useState } from "react";
+import React, { useState, MouseEvent } from "react";
 import styles from "./register.module.css";
 import axios from "axios";
 
 const Index = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const onRegister = async (e) => {
+  const onRegister = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
 
-    const user = {
-        email: email,
-        password: password
+    if (!email) {
+      setErrorMessage("No email");
+      setError(true);
+      return;
     }
+    if (!password) {
+      setErrorMessage("No password");
+      setError(true);
+      return;
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setErrorMessage("Invalid email format");
+      return;
+    }
+
+    const numberRegex = /\d/;
+    if (!numberRegex.test(password)) {
+      setErrorMessage("Password must include at least one number");
+      return;
+    }
+
+    const user = {
+      email: email,
+      password: password,
+    };
 
     try {
-      const response = await axios.post('http://localhost:3003/users', user);
-      console.log('Registration successful:', response.data);
+      const response = await axios.post("http://localhost:3003/users", user);
+      console.log("Registration successful:", response.data);
+      setError(false);
+      setSuccess(true);
     } catch (error) {
-      console.error('Registration failed:', error);
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 409) {
+          setError(true);
+          setErrorMessage("Email already in use");
+        } else {
+          console.error("Registration failed:", error);
+          setError(true);
+          setErrorMessage("Registration failed");
+        }
+      }
     }
-  };;
-
+  };
 
   return (
     <>
@@ -41,7 +76,8 @@ const Index = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
         <button onClick={onRegister}>Register</button>
-        <p>Error</p>
+        {error && <p className={styles.error}>{errorMessage}</p>}
+        {success && <p className={styles.success}>Account created</p>}
       </div>
     </>
   );
